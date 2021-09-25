e39207e2b223456b29e27ddaf60238ef8ed5d179
ThiwankaPrageeth <it20115234@my.sliit.lk>
2021-09-25 22:35:53 +0530

    Login and Menu added

--- app/src/main/java/com/example/dininggarage/MainActivity.java
+++ app/src/main/java/com/example/dininggarage/MainActivity.java
@@ -1,22 +1,61 @@
 package com.example.dininggarage;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login_btn, register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+        login_btn = findViewById(R.id.login_btn);
+        register_btn = findViewById(R.id.register_btn);
+
+        Log.i( "lifecycle", "onCreate method invoked");
+
+        login_btn.setOnClickListener(new View.OnClickListener(){
+
+                                         @Override
+                                         public void onClick(View v){
+
+                                             Intent Intent = new Intent (MainActivity.this,ConfirmLogin.class);
+                                             startActivity(Intent);
+
+                                             Log.i("lifecycle", "onClick method invoked");
+
+                                         }
+                                     }
+        );
+
+        register_btn.setOnClickListener(new View.OnClickListener(){
+
+                                            @Override
+                                            public void onClick(View v){
+
+                                                Intent Intent = new Intent (MainActivity.this,Register.class);
+                                                startActivity(Intent);
+
+                                                Log.i("lifecycle", "onClick method invoked");
+
+                                            }
+                                        }
+        );
+
+
+
+
     }
+
+
+
 }
