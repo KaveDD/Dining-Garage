9d622aff54e675e4f740306b866a0bf2e8fe0630
nethma-98 <nethmasahanpiyarathna@gmail.com>
2021-09-26 01:26:34 +0530

    contactus and feedback function added

---
+++ app/src/main/java/com/example/dininggarage/User.java
@@ -0,0 +1,34 @@
+package com.example.dininggarage;
+
+public class User {
+    private String Name;
+    private String Email;
+    private String Message;
+
+    public User() {
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+}
